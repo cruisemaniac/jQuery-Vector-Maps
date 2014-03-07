0c5adc9207e85565aae58e5e26d5727fa825d892
Ashwin Murali <mail@cruisemaniac.com>
2014-03-07 23:21:02 +0530

    changes to get the system working with IP

--- jqvmap.plugin.js
+++ jqvmap.plugin.js
@@ -5,15 +5,43 @@
  */
 
 
+
+var strip, strcountry, strcity, strregion, strlatitude, strlongitude, strtimezone;
 jQuery(document).ready(function($) {
-    
-    $('#vmap').vectorMap({
+
+$.getJSON('http://smart-ip.net/geoip-json/?callback=?', function(data) {
+    strip = data.host; strcountry = data.countryCode; strcity = data.city;
+    strregion = data.region; strlatitude = data.latitude; strlongitude = data.longitude;
+    strtimezone = data.timezone;
+    if(strcountry !== 'US') strregion = 'NY';
+
+ $('#vmap').vectorMap({
         map: 'usa_en',
         enableZoom: false,
+        backgroundColor: null,
         showTooltip: true,
-        selectedRegion: 'NY',
+        selectedRegion: (strregion !== null)? strregion : 'NY' ,
         onRegionClick: function(element, code, region) {
             window.location = region.replace(/ /g, "-");
         }
     });
 });
+});
+
+function GetUserInfo(data) {
+    strip = data.host; strcountry = data.countryName; strcity = data.city;
+    strregion = data.region; strlatitude = data.latitude; strlongitude = data.longitude;
+    strtimezone = data.timezone;
+    jQuery.holdReady(true);
+    //if(strcountry
+    vmap(true);
+
+}
+
+function vmap(us) {
+    if(us === false)
+       strregion = 'NY';
+
+    jQuery.holdReady(false);
+   
+}
