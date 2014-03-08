043edf873468795792cbd8c38ebf2cdde1a7dab9
Ashwin Murali <mail@cruisemaniac.com>
2014-03-08 13:10:32 +0530

    fixed state selection problem with long strings + added MIT license clause

--- jqvmap.plugin.js
+++ jqvmap.plugin.js
@@ -1,47 +1,56 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/*
+// The MIT License (MIT)
+// 
+// Copyright (c) 2014 Ashwin Murali
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+*/
+
+var strip, strcountry, strcity, strregion, strlatitude, strlongitude, strtimezone, statecode;
+var jsonstatestring = '{"states":[{"name":"Alabama","abbreviation":"AL"},{"name":"Alaska","abbreviation":"AK"},{"name":"American Samoa","abbreviation":"AS"},{"name":"Arizona","abbreviation":"AZ"},{"name":"Arkansas","abbreviation":"AR"},{"name":"California","abbreviation":"CA"},{"name":"Colorado","abbreviation":"CO"},{"name":"Connecticut","abbreviation":"CT"},{"name":"Delaware","abbreviation":"DE"},{"name":"District Of Columbia","abbreviation":"DC"},{"name":"Federated States Of Micronesia","abbreviation":"FM"},{"name":"Florida","abbreviation":"FL"},{"name":"Georgia","abbreviation":"GA"},{"name":"Guam","abbreviation":"GU"},{"name":"Hawaii","abbreviation":"HI"},{"name":"Idaho","abbreviation":"ID"},{"name":"Illinois","abbreviation":"IL"},{"name":"Indiana","abbreviation":"IN"},{"name":"Iowa","abbreviation":"IA"},{"name":"Kansas","abbreviation":"KS"},{"name":"Kentucky","abbreviation":"KY"},{"name":"Louisiana","abbreviation":"LA"},{"name":"Maine","abbreviation":"ME"},{"name":"Marshall Islands","abbreviation":"MH"},{"name":"Maryland","abbreviation":"MD"},{"name":"Massachusetts","abbreviation":"MA"},{"name":"Michigan","abbreviation":"MI"},{"name":"Minnesota","abbreviation":"MN"},{"name":"Mississippi","abbreviation":"MS"},{"name":"Missouri","abbreviation":"MO"},{"name":"Montana","abbreviation":"MT"},{"name":"Nebraska","abbreviation":"NE"},{"name":"Nevada","abbreviation":"NV"},{"name":"New Hampshire","abbreviation":"NH"},{"name":"New Jersey","abbreviation":"NJ"},{"name":"New Mexico","abbreviation":"NM"},{"name":"New York","abbreviation":"NY"},{"name":"North Carolina","abbreviation":"NC"},{"name":"North Dakota","abbreviation":"ND"},{"name":"Northern Mariana Islands","abbreviation":"MP"},{"name":"Ohio","abbreviation":"OH"},{"name":"Oklahoma","abbreviation":"OK"},{"name":"Oregon","abbreviation":"OR"},{"name":"Palau","abbreviation":"PW"},{"name":"Pennsylvania","abbreviation":"PA"},{"name":"Puerto Rico","abbreviation":"PR"},{"name":"Rhode Island","abbreviation":"RI"},{"name":"South Carolina","abbreviation":"SC"},{"name":"South Dakota","abbreviation":"SD"},{"name":"Tennessee","abbreviation":"TN"},{"name":"Texas","abbreviation":"TX"},{"name":"Utah","abbreviation":"UT"},{"name":"Vermont","abbreviation":"VT"},{"name":"Virgin Islands","abbreviation":"VI"},{"name":"Virginia","abbreviation":"VA"},{"name":"Washington","abbreviation":"WA"},{"name":"West Virginia","abbreviation":"WV"},{"name":"Wisconsin","abbreviation":"WI"},{"name":"Wyoming","abbreviation":"WY"}]}';
 
 
-
-var strip, strcountry, strcity, strregion, strlatitude, strlongitude, strtimezone;
 jQuery(document).ready(function($) {
 
 $.getJSON('http://smart-ip.net/geoip-json/?callback=?', function(data) {
     strip = data.host; strcountry = data.countryCode; strcity = data.city;
     strregion = data.region; strlatitude = data.latitude; strlongitude = data.longitude;
     strtimezone = data.timezone;
-    if(strcountry !== 'US') strregion = 'NY';
+    statecode = null;
+    var obj = $.parseJSON(jsonstatestring);
+    for(index in obj["states"])
+    {
+	if(obj["states"][index]["name"] === strregion)
+	    statecode = obj["states"][index]["abbreviation"];
+    }
+
+    if(strcountry !== 'US') statecode = 'NY';
 
  $('#vmap').vectorMap({
         map: 'usa_en',
         enableZoom: false,
         backgroundColor: null,
         showTooltip: true,
-        selectedRegion: (strregion !== null)? strregion : 'NY' ,
+        selectedRegion: (statecode !== null)? statecode : 'NY' ,
         onRegionClick: function(element, code, region) {
             window.location = region.replace(/ /g, "-");
         }
     });
 });
 });
-
-function GetUserInfo(data) {
-    strip = data.host; strcountry = data.countryName; strcity = data.city;
-    strregion = data.region; strlatitude = data.latitude; strlongitude = data.longitude;
-    strtimezone = data.timezone;
-    jQuery.holdReady(true);
-    //if(strcountry
-    vmap(true);
-
-}
-
-function vmap(us) {
-    if(us === false)
-       strregion = 'NY';
-
-    jQuery.holdReady(false);
-   
-}
